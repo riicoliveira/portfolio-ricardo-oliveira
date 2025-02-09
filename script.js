8925ea3475dbd3b35ee94ba50ee21c0dd9af5ff7
Your Name <you@example.com>
2025-02-09 20:20:18 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function toggleMode() {
+  const html = document.documentElement
+
+  // if (html.classList.contains("light")) {
+  //   html.classList.remove("light")
+  // } else {
+  //   html.classList.add("light")
+  // }
+
+  // outra maneira de fazer
+
+  html.classList.toggle('light')
+}
